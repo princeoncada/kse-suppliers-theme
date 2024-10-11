92001b5f36d6208fb3665b1ddca2914a6272a152
prince <prince.oncada@gmail.com>
2024-10-11 11:13:00 -0400

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    prefix: 'x-',
+	important: true,
+	content: [
+		"./**/*.liquid",
+		"./**/*.css",
+		"./**/*.html",
+		"./**/*.js"
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [],
+}

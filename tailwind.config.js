58bdc5f3a04596cb83f42db9d2f9303e52c109cc
prince <prince.oncada@gmail.com>
2024-10-22 21:41:55 -0400

    dev: optimized tailwind content watch to avoid node_modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     prefix: 'x-',
-	important: true,
-	content: [
-		"./**/*.liquid",
-		"./**/*.css",
-		"./**/*.html",
-	],
-	theme: {
-		extend: {},
+    important: true,
+    content: [
+        "./**/*.{html,liquid}",
+        "./assets/*.css",
+        "./assets/*.js"
+    ],
+    theme: {
+        extend: {},
         screens: {
             'md': '990px',
             'lg': '1280px'
         }
-	},
-	plugins: [],
+    },
+    plugins: [],
 }
